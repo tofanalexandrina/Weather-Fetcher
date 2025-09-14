b3d4caaa46f37e7a6a6b721c9453f61c7394315e
Tofan Alexandrina <tofanalexandrina22@stud.ase.ro>
2025-09-14 20:19:16 +0300

    user sees weather for multiple cities (loop added)

--- src/com/weatherfetcher/WeatherFetcher.java
+++ src/com/weatherfetcher/WeatherFetcher.java
@@ -14,31 +14,38 @@ public class WeatherFetcher {
     public static void main(String[] args) throws IOException, InterruptedException {
 
         Scanner scanner=new Scanner(System.in);
-        System.out.println("City: ");
+        while(true) {
+            System.out.println("City ('exit' to quit): ");
+            String city=scanner.nextLine();
 
-        HttpClient client= HttpClient.newHttpClient();
-        String city=scanner.nextLine();
-        String apiKey=System.getenv("WEATHER_API_KEY");
-        // /current.json - endpoint for current weather in JSON format(from documentation)
-        String url="https://api.weatherapi.com/v1/current.json?key="+apiKey+"&q="+city;
+            if(city.equalsIgnoreCase("exit")){
+                break;
+            }
+            HttpClient client= HttpClient.newHttpClient();
+            String apiKey=System.getenv("WEATHER_API_KEY");
+            // /current.json - endpoint for current weather in JSON format(from documentation)
+            String url="https://api.weatherapi.com/v1/current.json?key="+apiKey+"&q="+city;
 
-        //syntax breakdown:
-        // newBuilder - starts creating a new request
-        // .uri(URI.create(url)) - converts the URL string into a URI object
-        // ! Java's HTTP client works with URI(Uniform Resource Identifier), not raw strings
-        // .build() returns an HttpRequest usable object
-        HttpRequest request=HttpRequest.newBuilder().uri(URI.create(url)).build();
-        //the body of the response is read as text(String)
-        HttpResponse<String> response=client.send(request, HttpResponse.BodyHandlers.ofString());
+            //syntax breakdown:
+            // newBuilder - starts creating a new request
+            // .uri(URI.create(url)) - converts the URL string into a URI object
+            // ! Java's HTTP client works with URI(Uniform Resource Identifier), not raw strings
+            // .build() returns an HttpRequest usable object
+            HttpRequest request=HttpRequest.newBuilder().uri(URI.create(url)).build();
+            //the body of the response is read as text(String)
+            HttpResponse<String> response=client.send(request, HttpResponse.BodyHandlers.ofString());
 
-        //using Jackson to convert JSON response into Java obj
-        ObjectMapper mapper=new ObjectMapper();
-        WeatherResponse weatherResponse=mapper.readValue(response.body(), WeatherResponse.class);
+            //using Jackson to convert JSON response into Java obj
+            ObjectMapper mapper=new ObjectMapper();
+            WeatherResponse weatherResponse=mapper.readValue(response.body(), WeatherResponse.class);
 
-        //access the data
-        System.out.println("City: "+weatherResponse.location.name);
-        System.out.println("Country: "+weatherResponse.location.country);
-        System.out.println("Temperature: "+weatherResponse.current.temp_c+" °C");
-        System.out.println("Condition: "+weatherResponse.current.condition.text);
+            //access the data
+            System.out.println("City: "+weatherResponse.location.name);
+            System.out.println("Country: "+weatherResponse.location.country);
+            System.out.println("Temperature: "+weatherResponse.current.temp_c+" °C");
+            System.out.println("Condition: "+weatherResponse.current.condition.text);
+        }
+
+
     }
 }
